52a8552ae1e35290e029b2773eacc514cdabd772
LAUGIER Axel <axel.laugier@etu.univ-orleans.fr>
2016-11-04 16:20:42 +0100

    création de la classe Case

--- ModeleStratego/src/main/java/modele/Case.java
+++ ModeleStratego/src/main/java/modele/Case.java
@@ -4,4 +4,21 @@ package modele;
  * Created by Remy on 04/11/2016.
  */
 public class Case {
+    private int x;
+    private int y;
+    private boolean caseAccessible;
+
+    public Case(int x, int y, boolean caseAccessible) {
+        this.x = x;
+        this.y = y;
+        this.caseAccessible = caseAccessible;
+    }
+
+    public boolean estOccupe(int x, int y){
+        if()
+            return true;
+        else
+            return false;
+    }
 }
+
