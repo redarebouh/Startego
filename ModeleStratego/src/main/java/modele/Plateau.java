d565a91ec2b942bcbc63a3184520754f12673c19
Sainsard Remy <remy.sainsard@etu.univ-orleans.com>
2016-11-08 11:31:33 +0100

    modification du constructeur du plateau"

--- ModeleStratego/src/main/java/modele/Plateau.java
+++ ModeleStratego/src/main/java/modele/Plateau.java
@@ -7,51 +7,19 @@ import java.util.List;
  * Created by Remy on 04/11/2016.
  */
 public class Plateau {
-    private  Joueur joueurN1 ;
-
-    private Joueur joueurN2;
-
-    List<Joueur> joueurs = new ArrayList<Joueur>();
+    private Partie partie ;
 
     private Case [][] plateauStratego;
 
     private static final int TAILLE = 10;
 
-
-
-
-    public Plateau(List<Joueur> Joueurs){
-
-        this.joueurs = Joueurs;
-        plateauStratego = new Case[TAILLE][TAILLE];
+    public Plateau(Partie unePartie){
+        this.partie = unePartie;
+        this.plateauStratego = new Case[TAILLE][TAILLE];
     }
 
     public Case[][] getPlateauStratego(){
         return this.plateauStratego;
-    }
-
-    public Joueur getJoueurN1() {
-        return this.joueurN1;
-    }
-
-    public Joueur getJoueurN2(){
-        return this.joueurN2;
-    }
-
-    public void setJoueurN1(Joueur joueurN1){
-        this.joueurN1 = joueurN1;
-    }
-
-    public void setJoueurN2(Joueur joueurN2){
-        this.joueurN2 = joueurN2;
-    }
-
-    public List<Joueur> getJoueurs(){
-        return this.joueurs;
-    }
-
-    public void setJoueurs(List<Joueur>joueurs){
-        this.joueurs = joueurs;
     }
 
     public void getCaseParCoord(int x, int y,Piece unePiece){
